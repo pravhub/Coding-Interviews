d8186f8e15a25c8785b43fa8dfeef5ac3655f2b8
pravhub <58795189+pravhub@users.noreply.github.com>
2020-05-16 12:50:26 -0700

    Create lc328.cs

---
+++ lc328.cs
@@ -0,0 +1,48 @@
+public class Solution {
+    public ListNode OddEvenList(ListNode head) {
+        if(head==null || head.next==null || head.next.next==null)
+            return head;
+        ListNode temp = head;
+        ListNode oddStart = null;
+        ListNode oddEnd = null;
+        ListNode evenStart = null;
+        ListNode evenEnd = null;
+        int id = 1;
+        while(temp!=null)
+        {
+            if(id%2==1) //odd
+            {
+                if(oddStart==null)
+                {
+                    oddStart = temp;
+                    oddEnd = temp;
+                }
+                else
+                {
+                    oddEnd.next = temp;
+                    oddEnd = oddEnd.next;
+                }   
+                temp = temp.next;                        
+                oddEnd.next = null;
+            }
+            else
+            {
+                if(evenStart==null)
+                {
+                    evenStart = temp;
+                    evenEnd = temp;
+                }
+                else
+                {
+                    evenEnd.next = temp;
+                    evenEnd = evenEnd.next;
+                }          
+                temp = temp.next;    
+                evenEnd.next = null;
+            }
+            id++;
+        }
+        oddEnd.next = evenStart;
+        return oddStart;
+    }
+}
