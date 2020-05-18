d827bf8846f273f09975a086a935e9c39586b546
pravhub <58795189+pravhub@users.noreply.github.com>
2020-05-17 18:16:11 -0700

    Create lc438.java

---
+++ lc438.java
@@ -0,0 +1,58 @@
+class Solution {
+    public List<Integer> findAnagrams(String s, String p) {
+        List<Integer> ans = new ArrayList<Integer>();
+        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+        for(char c : p.toCharArray())
+        {
+            if(!map.containsKey(c))
+            {
+                map.put(c,0);
+            }
+            map.put(c,map.get(c)+1);
+        }
+        HashMap<Character, Integer> smap = new HashMap<Character, Integer>();
+        for(int i=0;i<s.length();i++)
+        {
+            if(!smap.containsKey(s.charAt(i)))
+            {
+                smap.put(s.charAt(i),1);
+            }            
+            else
+            {
+                smap.put(s.charAt(i),map.get(s.charAt(i))+1);
+            }
+            if(i>=p.length()-1)
+            {
+                if(CheckIfMapsAreEqual(map, smap))
+                {
+                    ans.add(i-p.length()+1);
+                }
+                char ch = s.charAt(i-p.length()+1);
+                int val = smap.get(ch);
+                smap.put(ch,val-1);
+                if(smap.get(ch)==0)
+                {
+                    smap.remove(ch);
+                }
+            }
+        }
+        return ans;
+    }
+    private boolean CheckIfMapsAreEqual(HashMap<Character, Integer> a, HashMap<Character, Integer> b)
+    {
+        if(a.size() != b.size())
+            return false;
+        for(char c: a.keySet())
+        {
+            if(b.containsKey(c) && a.get(c) ==b.get(c))
+            {
+                //we are good
+            }
+            else
+            {
+                return false;
+            }
+        }
+        return true;
+    }    
+}
