f9b00657bb31239ae0da5ef65d7c15f95b850298
pravhub <58795189+pravhub@users.noreply.github.com>
2021-12-24 21:07:12 -0800

    Update lc2113.py

--- lc2113.py
+++ lc2113.py
@@ -1,3 +1,5 @@
+#Leetcode 2113. Elements in Array After Removing and Replacing Elements
+#https://leetcode.com/problems/elements-in-array-after-removing-and-replacing-elements/
 class Solution:
     def elementInNums(self, nums: List[int], queries: List[List[int]]) -> List[int]:
         n = len(nums)
