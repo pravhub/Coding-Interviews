bafb39c74de77869c4ee5a95fbfc603d76c0e282
pravhub <58795189+pravhub@users.noreply.github.com>
2021-12-31 17:14:02 -0800

    Update lc1614.py

--- lc1614.py
+++ lc1614.py
@@ -1,7 +1,18 @@
 # https://leetcode.com/problems/maximum-nesting-depth-of-the-parentheses/
 # Leetcode  1614. Maximum Nesting Depth of the Parentheses
 class Solution:
-    def maxDepth(self, s: str) -> int:
+     def maxDepth(self, s: str) -> int:
+        cur = 0
+        ans = 0
+        for i, val in enumerate(s):
+            if(s[i] == ')'):
+                ans = max(ans, cur)
+                cur -= 1
+            elif(s[i] =='('):                    
+                cur += 1
+        return ans
+                
+    def maxDepth2(self, s: str) -> int:
         st = []
         ans = 0
         for i, val in enumerate(s):
