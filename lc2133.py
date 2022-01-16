86ace23f383228e30ee9a18ad5248387bc4733d5
pravhub <58795189+pravhub@users.noreply.github.com>
2022-01-15 16:12:38 -0800

    Update lc2133.py

--- lc2133.py
+++ lc2133.py
@@ -1,6 +1,10 @@
 # https://leetcode.com/problems/check-if-every-row-and-column-contains-all-numbers/
 # Leetcode 2133. Check if Every Row and Column Contains All Numbers
 class Solution:
+    def checkValid_Short(self, matrix: List[List[int]]) -> bool:
+    n = len(matrix)
+    return all(n == len(set(l)) for l in matrix) and all(n == len(set(l)) for l in zip(*matrix)) 
+
     def checkValid(self, matrix: List[List[int]]) -> bool:
         n = len(matrix)
         for l in matrix:
