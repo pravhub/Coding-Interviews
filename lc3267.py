44ad2166f7298f741d60d223e9f2f84421010ac7
pravhub <58795189+pravhub@users.noreply.github.com>
2022-09-06 21:53:25 -0700

    Create lc3267.py

---
+++ lc3267.py
@@ -0,0 +1,10 @@
+#brute force method
+class Solution:
+    def arithmeticTriplets(self, nums: List[int], diff: int) -> int:
+        count =0 
+        for i in range(0, len(nums)):
+            for j in range(i+1, len(nums)):
+                for k in range(j+1, len(nums)):
+                    if nums[j] - nums[i] == diff and nums[k] - nums[j] == diff:
+                        count += 1
+        return count
