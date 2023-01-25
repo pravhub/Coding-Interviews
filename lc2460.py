6faa88f1282fc566ee6e18aa494b379a45042f63
pravhub <58795189+pravhub@users.noreply.github.com>
2023-01-24 21:13:06 -0800

    Update lc2460.py

--- lc2460.py
+++ lc2460.py
@@ -6,16 +6,13 @@ class Solution:
                 nums[i+1] = 0
         
         
-        z_idx =0
-        while z_idx < len(nums):
-            while z_idx < len(nums) and nums[z_idx] != 0:
-                z_idx += 1 
-            idx = z_idx
-            while idx < len(nums) and nums[idx] == 0:
+        idx =0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[idx] = nums[i]
                 idx += 1
-            if z_idx < len(nums) and idx < len(nums):
-                nums[idx], nums[z_idx] = nums[z_idx], nums[idx]
-            idx+=1
-            z_idx+=1
+        while idx < len(nums):
+            nums[idx] = 0
+            idx +=1
                 
         return nums
