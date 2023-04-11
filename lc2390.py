c6e9cf8c98c512d3a0b4a9c4fc369d6919fd3598
pravhub <58795189+pravhub@users.noreply.github.com>
2023-04-11 11:26:09 -0700

    Create lc2390.py

---
+++ lc2390.py
@@ -0,0 +1,10 @@
+class Solution:
+    def removeStars(self, s: str) -> str:
+        st = []
+        for c in s:
+            if c == '*':
+                if len(st)>0:
+                    st.pop()
+            else:
+                st.append(c)
+        return ''.join(st)
